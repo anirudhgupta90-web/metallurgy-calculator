06eb96bf1b6485b8e0658a730b4dbb8a62c1a06f
anirudhgupta90-web <anirudhgupta90@gmail.com>
2026-04-04 15:39:04 +0530

    Update generate-codes.js

--- api/generate-codes.js
+++ api/generate-codes.js
@@ -15,6 +15,13 @@ function generateCode(length = 6) {
 }
 
 export default async function handler(req, res) {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
   try {
     const n = parseInt(req.query.n) || 5;
 
